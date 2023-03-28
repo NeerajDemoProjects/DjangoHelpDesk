cb687d4d9effef7bf58e6cef8f3238264ba8d1d9
erpsupporttecfuge <erpsupport@tecfuge.com>
2023-03-28 21:31:10 +0530

    remove repeated value

--- backend/ticket/views.py
+++ backend/ticket/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from email_configure.models import OutgoingEmailServer
 import smtplib
 from email.message import EmailMessage
+from django.db import IntegrityError
 
 
 def creation_email_ticket(EMAIL_TO):
@@ -20,7 +21,8 @@ def creation_email_ticket(EMAIL_TO):
     msg['Subject'] = 'Grab Dinner this weekend'
     msg['From'] = EMAIL_ADDRESS
     msg['To'] =EMAIL_TO
-    msg.set_content('yes i am tony')
+    html_msg = "<br> <b><u> This is the Text .... </u></b><br>"
+    msg.add_alternative(html_msg, subtype="html")
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASS)
@@ -32,13 +34,22 @@ class CreateTicket(APIView):
         serializer = TicketCreateSerializer(data=request.data)
 
         if serializer.is_valid():
-            state_id = Status.objects.get(name="New")
-            contact,var =Contact.objects.get_or_create(name=serializer.data['name'],email=serializer.data['email'],
-                                                       phone =serializer.data['phone']
+            try :
+                state_id = Status.objects.get(name="New")
+                contact,var =Contact.objects.get_or_create(name=serializer.data['name'],email=serializer.data['email'],
+                                                           phone =serializer.data['phone']
 
-                                                       )
-            Ticket.objects.create(contact_id=contact,query=serializer.data['query'],state_id=state_id)
-            creation_email_ticket(contact.email)
+                                                           )
+                Ticket.objects.create(contact_id=contact,query=serializer.data['query'],state_id=state_id)
+                creation_email_ticket(contact.email)
+                return Response(serializer.data, status=400)
+
+            except IntegrityError as e:
+                contact = contact.objects.get(email=serializer.data['email'])
+                creation_email_ticket(contact.email)
+                return Response(serializer.data, status=400)
+
+
             return Response(serializer.data, status=400)
 
 
